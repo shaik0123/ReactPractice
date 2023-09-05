24018fc82d0fa833981d012df8783092a32103ee
shaik0123 <shaik.ismail8563@gmail.com>
2023-09-06 00:08:30 +0530

    [Ismail] Refactor : Implemented Regex Validations for input feilds

--- fundoo-react/src/Components/Forms/SignIn.js
+++ fundoo-react/src/Components/Forms/SignIn.js
@@ -9,41 +9,84 @@ import { useState } from "react";
 
 
 function SignIn() {
-    const [ userlogin,setuserlogin]=useState({
-        email:"",
-        password:""
+    const [userlogin, setuserlogin] = useState({
+        email: "",
+        password: ""
     });
-    let name,value;
-    const input = (e) =>{
+    let name, value;
+    const input = (e) => {
         name = e.target.name;
         value = e.target.value;
-        setuserlogin({...userlogin,[name]:value})
-    }; 
-
-    const handlelogin = (e) =>{
-        console.log(userlogin); 
-        alert("user registration successfull");
+        setuserlogin({ ...userlogin, [name]: value })
     };
+    const emailRegex = /^[a-z]{3,}(.[0-9a-z]*)?@([a-z]){2,}.[a-z]+(.in)*$/;
+    const passwordRegex = /^.*(?=.{8,})(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+=]).*$/;
+    const [errorObj, setErrorObj] = useState({
+        emailError: false,
+        emailHelper: "",
+        passwordError: false,
+        passwordHelper: "",
+    })
+
+    const handlelogin = (e) => {
+        // console.log(userlogin);
+        // alert("user registration successfull");
+        let emailTest = emailRegex.test(userlogin.email);
+        let passwordTest = passwordRegex.test(userlogin.password);
+        if (emailTest == false) {
+            setErrorObj((prev) => ({
+                ...prev,
+                emailError: true,
+                emailHelper: "enter correct email"
+            }));
+        }
+        else {
+            setErrorObj((prev) => ({
+                ...prev,
+                emailError:false,
+                emailHelper: ""
+            }));
+
+        }
+        if (passwordTest == false) {
+            setErrorObj((prev) => ({
+                ...prev,
+                passwordError: true,
+                passwordHelper: "enter correct password"
+            }));
+        }
+        else {
+            setErrorObj((prev) => ({
+                ...prev,
+                passwordError:false,
+                passwordHelper: ""
+            }));
+
+        }
+        console.log(userlogin)
+    };
+
+   
 
     return (
         <div className="main">
             <div className="Loginform">
-                <img className="imgin"  src={fundoologo} alt="logo"></img>
+                <img className="imgin" src={fundoologo} alt="logo"></img>
                 <h1>Sign in</h1>
 
                 <p>with your fundoo account</p>
                 <div className="emailpassinput">
-                    <TextField className="standard-basic"  label="Email" variant="outlined" required name="email" onChange={input} value={userlogin.email} />
+                    <TextField className="standard-basic" label="Email" variant="outlined" required name="email" onChange={input} value={userlogin.email} error={errorObj.emailError} helperText={errorObj.emailHelper} />
                 </div>
                 <div className="emailpassinput">
-                    <TextField className="standard-basic" type="password" label="password" variant="outlined" required name="password" value={userlogin.password} onChange={input}/>
+                    <TextField className="standard-basic" type="password" label="password" variant="outlined" required name="password" value={userlogin.password} error={errorObj.passwordError} helperText={errorObj.passwordHelper} onChange={input} />
                 </div>
                 <div className="inputPass">
                     <Link className="anchor" to="">Forgot Password?</Link>
                 </div>
                 <div className="bottom">
                     <Link className="signupbtn" to="/SignUp">Create account</Link>
-                    <Button className="btn" variant="contained"  onClick={handlelogin}>Login</Button>
+                    <Button className="btn" variant="contained" onClick={handlelogin}>Login</Button>
                 </div>
             </div>
         </div>
--- fundoo-react/src/Components/Forms/SignUp.js
+++ fundoo-react/src/Components/Forms/SignUp.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import '../../Components/Css/SignUp.css';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
@@ -10,27 +10,146 @@ import { FormControlLabel } from "@mui/material";
 
 
 function SignUp() {
+
+    const [userSignUp, setuserSignUp] = useState({
+        firstname: "",
+        lastname: "",
+        email: "",
+        password: "",
+        confirmpassword: ""
+    });
+    let names, values;
+    const signupinput = (e) => {
+        names = e.target.name;
+        values = e.target.value;
+        setuserSignUp({ ...userSignUp, [names]: values })
+    };
+    const firstnameRegex = /^[a-zA-Z ]{2,30}$/;
+    const lastnameRegex = /^[a-zA-Z ]{2,30}$/;
+    const emailRegex = /^[a-z]{3,}(.[0-9a-z]*)?@([a-z]){2,}.[a-z]+(.in)*$/;
+    const passwordRegex = /^.*(?=.{8,})(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+=]).*$/;
+    const confirmpasswordRegex = /^.*(?=.{8,})(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+=]).*$/;
+
+    const [errorObj, setErrorObj] = useState({
+        firstnameError:false,
+        firstnameHelper:"",
+        lastnameError:false,
+        lastnameHelper:"",
+        emailError: false,
+        emailHelper: "",
+        passwordError: false,
+        passwordHelper: "",
+        confirmpasswordError:false,
+        confirmpasswordHelper:""
+    })
+
+    const handlesignup = (e) => {
+        // console.log(userSignUp);
+        // alert("user registration successfull");
+        let firstnameTest = firstnameRegex.test(userSignUp.firstname);
+        let lastnameTest = lastnameRegex.test(userSignUp.lastname);
+        let emailTest = emailRegex.test(userSignUp.email);
+        let passwordTest =passwordRegex.test(userSignUp.password);
+        let confirmpasswordTest = confirmpasswordRegex.test(userSignUp.comfirmpassword);
+        if (firstnameTest == false) {
+            setErrorObj((prev) => ({
+                ...prev,
+                firstnameError: true,
+                firstnameHelper: "enter correct first name"
+            }));
+        }
+        else {
+            setErrorObj((prev) => ({
+                ...prev,
+                firstnameError:false,
+                firstnameHelper: ""
+            }));
+
+        }
+        if (lastnameTest == false) {
+            setErrorObj((prev) => ({
+                ...prev,
+                lastnameError: true,
+                lastnameHelper: "enter correct last name"
+            }));
+        }
+        else {
+            setErrorObj((prev) => ({
+                ...prev,
+                lastnameError:false,
+                lastnameHelper: ""
+            }));
+
+        }
+        if (emailTest == false) {
+            setErrorObj((prev) => ({
+                ...prev,
+                emailError: true,
+                emailHelper: "enter correct email"
+            }));
+        }
+        else {
+            setErrorObj((prev) => ({
+                ...prev,
+                emailError:false,
+                emailHelper: ""
+            }));
+
+        }
+        if (passwordTest == false) {
+            setErrorObj((prev) => ({
+                ...prev,
+                passwordError: true,
+                passwordHelper: "enter correct password"
+            }));
+        }
+        else {
+            setErrorObj((prev) => ({
+                ...prev,
+                passwordError:false,
+                passwordHelper: ""
+            }));
+
+        }
+        if (confirmpasswordTest == false) {
+            setErrorObj((prev) => ({
+                ...prev,
+                confirmpasswordError: true,
+                confirmpasswordHelper: "enter correct password"
+            }));
+        }
+        else {
+            setErrorObj((prev) => ({
+                ...prev,
+                confirmpasswordError:false,
+                confirmpasswordHelper: ""
+            }));
+
+        }
+        console.log(userSignUp);
+    }
     return (
         <div className="outer">
             <div className="maindiv">
-
                 <div className="feilds">
                     <div><img src={fundoologoup} className="imgup" alt="logo"></img></div>
                     <h1>Create Your Fundoo Account</h1>
                     <div className="FnLn">
-                        <div><TextField className="Fname" label="First name" variant="outlined" required /></div>
-                        <div><TextField className="Lname" label="Last name" variant="outlined" required /></div>
+                        <div><TextField className="Fname" label="First name" variant="outlined" required name="firstname" value={userSignUp.firstname} onChange={signupinput} error={errorObj.firstnameError} helperText={errorObj.firstnameHelper} /></div>
+
+                        <div><TextField className="Lname" label="Last name" variant="outlined" required name="lastname" value={userSignUp.lastname} onChange={signupinput} error={errorObj.lastnameError} helperText={errorObj.lastnameHelper}/></div>
                     </div>
                     <div className="email">
-                        <TextField className="emailfeild" fullWidth type="Email" label="Email" variant="outlined" required />
+                        <TextField className="emailfeild" fullWidth type="Email" label="Email" variant="outlined" required name="email" value={userSignUp.email} onChange={signupinput} error={errorObj.emailError} helperText={errorObj.emailHelper}/>
                     </div>
                     <div className="p">
                         <p>You can use letters,numbers & periods</p>
                         <p>Use my current email insted</p>
                     </div>
                     <div className="FnLn">
-                        <div><TextField id="outlined-p" type="password" label="Password" variant="outlined" required /></div>
-                        <div><TextField className="conpass" id="outlined-cp" type="password" label="Confirm Password" variant="outlined" required /></div>
+                        <div><TextField id="outlined-p" type="password" label="Password" variant="outlined" required name="password" value={userSignUp.password} onChange={signupinput} error={errorObj.passwordError} helperText={errorObj.passwordHelper}/></div>
+
+                        <div><TextField className="conpass" id="outlined-cp" type="password" label="Confirm Password" variant="outlined" required name="comfirmpassword" value={userSignUp.comfirmpassword} onChange={signupinput} error={errorObj.confirmpasswordError} helperText={errorObj.confirmpasswordHelper}/></div>
                     </div>
                     <div className="showpass">
                         <p>Use 8 or more characters with a mix of letters,numbers &symbols</p>
@@ -38,7 +157,7 @@ function SignUp() {
                     </div>
                     <div className="btns">
                         <div><Link className="Link" to="/SignIn">Sign in insted</Link></div>
-                        <div><Button variant="contained">Next</Button></div>
+                        <div><Button variant="contained" onClick={handlesignup}>Next</Button></div>
                     </div>
                 </div>
                 <div className="img">
